53791686320c44581758868fef6b8b48a22ba035
124476 <124476@bk.ru>
2024-07-29 16:09:04 +0300

    Uplode program

---
+++ Macdonalds/App.xaml.cs
@@ -0,0 +1,29 @@
+﻿using Macdonalds.Models;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Macdonalds
+{
+    /// <summary>
+    /// Логика взаимодействия для App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        public static MacdonaldsDatabaseEntities DB = new MacdonaldsDatabaseEntities();
+        public App()
+        {
+            DispatcherUnhandledException += App_DispatcherUnhandledException;
+        }
+
+        private void App_DispatcherUnhandledException(object sender, System.Windows.Threading.DispatcherUnhandledExceptionEventArgs e)
+        {
+            e.Handled = true;
+            MessageBox.Show("Error");
+        }
+    }
+}
